d6f2bc0cc7e08b0d9ead2c3dc28fe88b49baf0df
almasoud49 <102854436+almasoud49@users.noreply.github.com>
2023-03-03 22:32:39 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
 
+console.log('Hello Guys! How are You');
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Learn MERN Like a Pro</h1>`;
